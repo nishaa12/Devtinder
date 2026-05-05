5096b154a48c32603445b7e421a5be731b612a07
nishaa12 <nishakrim40@gmail.com>
2026-05-05 06:09:11 +0530

    multiple route handler

--- src/app.js
+++ src/app.js
@@ -2,9 +2,42 @@ const express=require("express");
 
 const app=express();
 
-app.use("/home",(req,res)=>{
-    res.send("hiii nishuuu");
-});
+
+//app.use("/home",(req,res)=>{
+   // res.send("hiii nishuuu");
+//});
+//app.use("/home123",(req,res)=>{
+    //res.send("hiii 123");
+//});
+
+//app.get("/test/:userid/:name/:password",(req,res)=>{
+    //console.log(req.params);
+    //res.send("hello from test");
+//});
+//app.get("/",(req,res)=>{
+    //res.send("hii ");
+//});
+
+
+//Route handlers
+ app.use("/user",
+    (req,res,next)=>{
+    console.log("route handler 1");
+    //res.send("response1");
+    next();
+   // res.send("response1");
+},
+(req,res,next)=>{
+    console.log("route handler from user 2");
+    //res.send("response2");
+    next();
+},
+(req,res,next)=>{
+    console.log("route handler from user 3");
+  res.send("response3");
+}
+
+);
 
 app.listen(7777,()=>{
     console.log("hiii from server 3000");
