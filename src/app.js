eda3997fdba998f826c8a34b66f260845d149b16
nishaa12 <nishakrim40@gmail.com>
2026-05-21 06:12:37 +0530

    get and post api

--- src/app.js
+++ src/app.js
@@ -5,25 +5,42 @@ const app=express();
 const connectDB=require("./config/database");
 
 const User=require("./models/user")
-
-
+//post api
+app.use(express.json());
 app.post("/signup",async(req,res)=>{
-    const user=new User({
-     firstName:"Nisha",
-     lastName:"Bharti",
-     age:25,
-     password:"Nisha",
-
-    });
+    const user=new User(req.body);
+    console.log(req.body);
 
     try{
-    await user.save();
-    res.send("User created");
+    await user.save()
+    res.send("user created");
     }
     catch(err){
-        res.status(400).send("user is not created"+err);
+      res.status(400).send("something went wrong");
     }
 });
+
+//get api
+
+app.get("/getapi",async(req,res)=>{
+
+    const userEmail=req.body.emailid;
+    
+    try{
+      const user = await User.find({emailid:userEmail});
+         res.send("user");    
+    }
+    catch(err){
+          res.status(400).send("user not found");
+
+    }
+
+
+     
+});
+
+
+
 
 connectDB().then(()=>{
     console.log("database connected succesfully");
--- src/models/user.js
+++ src/models/user.js
@@ -13,6 +13,9 @@ const userSchema=new mongoose.Schema({
     },
     password:{
         type:String
+    },
+    emailid:{
+     type:String
     }
 }
 );
